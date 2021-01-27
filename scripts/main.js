40379e633146d1b0fbaeeef5f037887eacc3b7d5
Gitthuma <george.githuma@gmail.com>
2021-01-28 00:17:00 +0300

    Added code to flip images between firefox-con and firefox-icon2

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.jpeg') {
+        myImage.setAttribute('src', 'images/firefox-icon2.jpeg')
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.jpeg')
+    }
+}
